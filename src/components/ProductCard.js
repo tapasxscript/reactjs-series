7fd32ba49d7ba5158d42f05747e97d8c2113fc2d
tapasxscript <tapasranjanbehera2604@gmail.com>
2025-08-14 15:04:21 +0530

    Update ProductCard.js with brief summary

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,53 @@
-import React from "react";
-import { productList } from "../utils/constant";
+import React,{useState,useEffect} from "react";
 import Product from "./Product";
+import Skeleton from "./Skeleton";
 
-const ProductCard=()=>{
-    return(
-      <div className='product_card'>
-        {
-          productList.map((product,index)=>{
-            return(
-            <Product key={product.id} product={product}/>
-          )
-        })
-        }
-      </div>
+const ProductCard = () => {
+    const [listOfProduct,setListOfProduct] = useState([])
+    const [filterProduct,setFilterProduct] = useState([])
+    const [searchText,setSearchText] = useState("")
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData= async()=>{
+        const data = await fetch(`https://fakestoreapi.com/products`);
+        const resdata = await data.json();
+        // console.log(resdata);
+        setListOfProduct(resdata);
+        setFilterProduct(resdata);
+    }
+
+    const display=()=>{
+        const value = listOfProduct.filter(product=> product.rating.rate>=4)
+        setListOfProduct(value)
+    }
+
+    const filterShow=()=>{
+            const filterData = listOfProduct.filter((product)=>{
+               return product.title.toLowerCase().includes(searchText.toLowerCase())
+            });
+            setFilterProduct(filterData);
+    }
+
+    return listOfProduct.length===0 ? <Skeleton/> : (
+        <div>
+            <div>
+                <input type="text" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+                <button onClick={filterShow}>search</button>
+            </div>
+            <button onClick={display} style={{ "marginTop": "10px" }}>TopRated Product</button>
+            <div className='product_card'>
+                {
+                    filterProduct.map((product, index) => {
+                        return (
+                            <Product key={product.id} product={product} />
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
-  }
-  export default ProductCard;
+}
+export default ProductCard;
