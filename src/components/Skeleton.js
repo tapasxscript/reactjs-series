24e700507a32eb11ea63c1e6b32f6856c3339ef6
tapasxscript <tapasranjanbehera2604@gmail.com>
2025-08-14 14:57:47 +0530

    Add Skeleton.js

---
+++ src/components/Skeleton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skeleton = () => {
+  return (
+    <div className='skeleton-container'>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+    </div>
+  )
+}
+
+export default Skeleton
